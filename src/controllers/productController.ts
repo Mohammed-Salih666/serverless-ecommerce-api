23e010363222feb138a9617c5ffd3e8035024875
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2024-01-23 22:37:40 +0300

    Create a Product Controller

---
+++ src/controllers/productController.ts
@@ -0,0 +1,35 @@
+import {Product} from 'src/database/product'; 
+
+export const getAllProducts = async(): Promise<Product[]> => {
+    const prdouctsRecord = await Product.getAll(); 
+    const products: Product[] = []; 
+
+    prdouctsRecord.forEach(record => {
+        const product = Product.fromItem(record); 
+        products.push(product);
+    }); 
+
+    return products; 
+}
+
+export const getProduct = async (name: string): Promise<Product> => {
+    const product = await Product.get(name); 
+    return product; 
+}
+
+export const createProduct = async (product: Product): Promise<Product> => {
+    const createdProduct = await Product.create(product); 
+    return createdProduct; 
+}
+
+export const updateProduct = async(name: string, attribute: string, value: string): Promise<Product> => {
+    if(attribute == "pk" || attribute == "sk" || attribute == "name") throw new Error(`Cannot update ${attribute}. It is part of the primary key`); 
+    
+    const product = await Product.update(name, attribute, value); 
+    return product; 
+}
+
+export const deleteProduct = async (name: string): Promise<Product> => {
+    const product = await Product.deleteProduct(name); 
+    return product;
+}
